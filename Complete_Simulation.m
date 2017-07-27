950b02e04323100b4fc4dfa53d7917f89722dca2
aasim74 <zcemapa@ucl.ac.uk>
2017-07-27 18:40:02 +0100

    Plotted 2D graph of incoming wave - surf plot, neatened code with comments and spacing

--- Complete_Simulation.m
+++ Complete_Simulation.m
@@ -2,48 +2,33 @@
 %before reaching the aperture and then animating the wave after it passes
 %through the aperture and hitting the screen
 
-%Define the variables
+%Define the variables------------------------------------------------------
 c = 3e8;
 lambda = 630e-9;
 f = c/lambda;
 k = 2*pi/lambda;
 omega = 2*pi*f;
 A = 1;
-t=0;
 
-x = linspace(-15e-6, 15e-6, 501);
-y = linspace(-15e-6, 15e-6, 501);
+x = linspace(-1e-6, 1e-6, 201);
+y = linspace(-1e-6, 1e-6, 201);
+t= 0: 1: 10;
 
+
+%Creates a 2-D matrix of intensities at different x and y values-----------
 incoming_wave = zeros(length(x), length(y));
 
-size(incoming_wave)
-
-%Creates a 2-D matrix of intensities at different x and y values
 for i = 1: 1: length(x)
     for j = 1: 1: length(y)
-        incoming_wave(i,j) = A*exp(1j*k*(x(i)+y(j)) - omega*t);
-    end
-end
-
-size(incoming_wave)
-
-t = 0: 1: 100;
-
-%Creates a 3-D array with time being the third dimension
-for l = 1: 1: length(t)
-    incoming_wave(:, :, l) = A*exp(1j*k*(x(i)+y(j)) - omega*t(l));
-end
-
-figure(1);
-
-%Plot the wave as a function of x-y position and evolving with time t
-for i = 1: 1: length(x)
-    for j = 1: 1: length(y)
-        for t=0
-            surf(x, y, real(incoming_wave))
+        for l = 1: 1: length(t)
+        incoming_wave(i, j, l) = A*exp(1j*k*(x(i)+y(j) - omega*t(l)));
         end
     end
 end
 
-
-size(incoming_wave)
+%Plot 2-D graph------------------------------------------------------------
+figure(1);
+surf(x, y, real(incoming_wave(:,:,1)))
+xlabel('x-axis');
+ylabel('y-axis');
+zlabel('Amplitude');
